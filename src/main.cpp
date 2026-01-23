56c0a078494f449a044af6eba4e15749a9a0a862
fzybot <ruslan.akhpashev@gmail.com>
2026-01-23 13:59:30 +0700

    Docking over ViewPort is enabled.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
-#include <iostream>
-
 #include <GL/glew.h>
 #include <SDL2/SDL.h>
 
+#include <iostream>
+#include <chrono>
+#include <thread>
 #include <cmath>
 
 #include "backends/imgui_impl_opengl3.h"
@@ -11,22 +12,31 @@
 #include "implot.h"
 
 
+
 int main(int argc, char *argv[]) {
 
-    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
     SDL_Window* window = SDL_CreateWindow(
         "Backend start", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         1024, 768, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
     SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-    glewInit();
+
     ImGui::CreateContext();
     ImPlot::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+    // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+
     ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
     ImGui_ImplOpenGL3_Init("#version 330");
 
     bool running = true;
     // auto last_frame_time = std::chrono::steady_clock::now();
     while (running) {
+
+        // Poll and handle events (inputs, window resize, etc.)
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             ImGui_ImplSDL2_ProcessEvent(&event);
@@ -34,15 +44,42 @@ int main(int argc, char *argv[]) {
                 running = false;
             }
         }
+
+        // Start the Dear ImGui frame
         ImGui_ImplOpenGL3_NewFrame();
         ImGui_ImplSDL2_NewFrame();
         ImGui::NewFrame();
+        ImGui::DockSpaceOverViewport(0, nullptr, ImGuiDockNodeFlags_None);
+
+        //1. 
+        {
+            static float f = 0.0f;
+            static int counter = 0;
+
+            ImGui::Begin("Hello, world!"); 
+            ImGui::Text("This is some useful text.");    
+            if (ImGui::Button("Button"))                         
+                counter++;
+            ImGui::Text("counter = %d", counter);
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+        {
+
+            ImGui::Begin("Second window for dock!"); 
+            ImGui::Text("This is some useful text.");    
+            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
 
 
         ImGui::Render();
         glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         SDL_GL_SwapWindow(window);
     }
 
