8650b5040ac1dea76ad7e41e8d0b2ed8215cdd2a
fzybot <ruslan.akhpashev@gmail.com>
2026-01-23 15:56:31 +0700

    ImPlot test success.

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,45 @@
 #include "imgui.h"
 #include "implot.h"
 
+struct ScrollingBuffer {
+    int MaxSize;
+    int Offset;
+    ImVector<ImVec2> Data;
+    ScrollingBuffer(int max_size = 2000) {
+        MaxSize = max_size;
+        Offset  = 0;
+        Data.reserve(MaxSize);
+    }
+    void AddPoint(float x, float y) {
+        if (Data.size() < MaxSize)
+            Data.push_back(ImVec2(x,y));
+        else {
+            Data[Offset] = ImVec2(x,y);
+            Offset =  (Offset + 1) % MaxSize;
+        }
+    }
+    void Erase() {
+        if (Data.size() > 0) {
+            Data.shrink(0);
+            Offset  = 0;
+        }
+    }
+};
+
+struct RollingBuffer {
+    float Span;
+    ImVector<ImVec2> Data;
+    RollingBuffer() {
+        Span = 10.0f;
+        Data.reserve(2000);
+    }
+    void AddPoint(float x, float y) {
+        float xmod = fmodf(x, Span);
+        if (!Data.empty() && xmod < Data.back().x)
+            Data.shrink(0);
+        Data.push_back(ImVec2(xmod, y));
+    }
+};
 
 
 int main(int argc, char *argv[]) {
@@ -64,14 +103,65 @@ int main(int argc, char *argv[]) {
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
             ImGui::End();
         }
+        // ImPlot::ShowDemoWindow();
+
+        // {
+        //     ImGui::Begin("Test Plot");
+        //     float samples[1000];
+        //     for (int n = 0; n < 1000; n++)
+        //         samples[n] = sinf(n * 0.2f + ImGui::GetTime());
+        //     ImGui::PlotLines("Samples", samples, 1000, 0, NULL, 0.0f, 1.0f, ImVec2(1000, 600));
+
+        //     // Display contents in a scrolling region
+        //     ImGui::TextColored(ImVec4(1,1,0,1), "Important Stuff");
+        //     ImGui::End();
+        // }
 
         {
+            ImGui::Begin("Hello, Plots!@"); 
+            static ScrollingBuffer sdata1, sdata2;
+            static RollingBuffer   rdata1, rdata2;
+            ImVec2 mouse = ImGui::GetMousePos();
 
-            ImGui::Begin("Second window for dock!"); 
-            ImGui::Text("This is some useful text.");    
-            ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            // Add points to the buffers every 0.02 seconds
+            static float t = 0, last_t = 0.0f;
+            if (t == 0 || t - last_t >= 0.02f) {
+                sdata1.AddPoint(t, mouse.x * 0.0005f);
+                rdata1.AddPoint(t, mouse.x * 0.0005f);
+                sdata2.AddPoint(t, mouse.y * 0.0005f);
+                rdata2.AddPoint(t, mouse.y * 0.0005f);
+                last_t = t;
+            }
+            t += ImGui::GetIO().DeltaTime;
+
+            static float history = 10.0f;
+            ImGui::SliderFloat("History",&history,1,30,"%.1f s");
+            rdata1.Span = history;
+            rdata2.Span = history;
+
+            static ImPlotAxisFlags flags = ImPlotAxisFlags_NoTickLabels;
+
+            if (ImPlot::BeginPlot("##Scrolling", ImVec2(-1,ImGui::GetTextLineHeight()*10))) {
+                ImPlot::SetupAxes(nullptr, nullptr, flags, flags);
+                ImPlot::SetupAxisLimits(ImAxis_X1,t - history, t, ImGuiCond_Always);
+                ImPlot::SetupAxisLimits(ImAxis_Y1,0,1);
+                ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL,0.5f);
+                ImPlot::PlotShaded("Mouse X", &sdata1.Data[0].x, &sdata1.Data[0].y, sdata1.Data.size(), -INFINITY, 0, sdata1.Offset, 2 * sizeof(float));
+                ImPlot::PlotLine("Mouse Y", &sdata2.Data[0].x, &sdata2.Data[0].y, sdata2.Data.size(), 0, sdata2.Offset, 2*sizeof(float));
+                ImPlot::EndPlot();
+            }
+            if (ImPlot::BeginPlot("##Rolling", ImVec2(-1,ImGui::GetTextLineHeight()*10))) {
+                ImPlot::SetupAxes(nullptr, nullptr, flags, flags);
+                ImPlot::SetupAxisLimits(ImAxis_X1,0,history, ImGuiCond_Always);
+                ImPlot::SetupAxisLimits(ImAxis_Y1,0,1);
+                ImPlot::PlotLine("Mouse X", &rdata1.Data[0].x, &rdata1.Data[0].y, rdata1.Data.size(), 0, 0, 2 * sizeof(float));
+                ImPlot::PlotLine("Mouse Y", &rdata2.Data[0].x, &rdata2.Data[0].y, rdata2.Data.size(), 0, 0, 2 * sizeof(float));
+                ImPlot::EndPlot();
+            }
             ImGui::End();
         }
+
+
 
 
 
