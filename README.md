281b7f14b27851fa4ee2222a49a133f948bc3dbe
fzybot <ruslan.akhpashev@gmail.com>
2026-01-29 22:11:41 +0700

    hotfix

--- README.md
+++ README.md
@@ -7,8 +7,7 @@ sudo apt install libsdl2-dev libgl1-mesa-dev libglew-dev
 
 Клонируем:
 ```bash
-git clone https://github.com/TelecomDep/backend_notes.git
-git submodule update --recursive
+git clone --recursive https://github.com/TelecomDep/backend_notes.git
 ```
 
 Сборка\запуск:
--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,14 @@
 #include "imgui.h"
 #include "implot.h"
 
+template <typename T>
+inline T RandomRange(T min, T max) {
+    T scale = rand() / (T) RAND_MAX;
+    return min + scale * ( max - min );
+}
+
+
+
 void run_gui(){
     SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
     SDL_Window* window = SDL_CreateWindow(
@@ -49,8 +57,41 @@ void run_gui(){
         ImGui::NewFrame();
         ImGui::DockSpaceOverViewport(0, nullptr, ImGuiDockNodeFlags_None);
 
+        //1. 
+        {
+            static int counter = 0;
 
-        ImGui::ShowDemoWindow();
+            ImGui::Begin("Hello, world!");
+            if (ImGui::Button("Button"))
+                counter++;
+            ImGui::Text("counter = %d", counter);
+            // ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+            ImGui::End();
+        }
+
+        // ImGui::ShowDemoWindow();
+        // ImPlot::ShowDemoWindow();
+        // ImGui::Begin("Mouse Position");
+        // ImVec2 mouse = ImGui::GetMousePos();
+        // ImGui::Text("Mouse position: x = %f, y = %f", mouse.x, mouse.y);
+        // ImGui::End();
+
+        // {
+        //     ImGui::Begin("My Plot!");
+        //     double data_x[1000];
+        //     double data_y[1000];
+        //     for (int i = 0; i < 1000; i++){
+        //         data_x[i] = RandomRange(400.0,450.0);
+        //         data_y[i] = RandomRange(100.0,150.0);
+        //     }
+        //     if(ImPlot::BeginPlot("##Scrolling"))
+        //     {
+        //         ImPlot::PlotScatter("Mouse X", &data_x[0], &data_y[0], 1000, 0, 0, 2 * sizeof(float));
+        //         ImPlot::EndPlot();
+        //     }
+        
+        //     ImGui::End();
+        // }
 
 
         ImGui::Render();
@@ -77,3 +118,48 @@ int main(int argc, char *argv[]) {
     gui_thread.join();
     return 0;
 }
+
+        // Cool plots
+        // {
+        //     ImGui::Begin("Hello, Plots!");
+        //     static ScrollingBuffer sdata1, sdata2;
+        //     static RollingBuffer rdata1, rdata2;
+        //     // Add points to the buffers every 0.02 seconds
+        //     static float t = 0, last_t = 0.0f;
+        //     if (t == 0 || t - last_t >= 0.001f)
+        //     {
+        //         sdata1.AddPoint(t, mouse.x * 0.0005f);
+        //         rdata1.AddPoint(t, mouse.x * 0.0005f);
+        //         sdata2.AddPoint(t, mouse.y * 0.0005f);
+        //         rdata2.AddPoint(t, mouse.y * 0.0005f);
+        //         last_t = t;
+        //     }
+        //     t += ImGui::GetIO().DeltaTime;
+
+        //     static float history = 10.0f;
+        //     ImGui::SliderFloat("History",&history,1,30,"%.1f s");
+        //     rdata1.Span = history;
+        //     rdata2.Span = history;
+
+        //     static ImPlotAxisFlags flags = ImPlotAxisFlags_NoTickLabels;
+
+        //     if (ImPlot::BeginPlot("##Scrolling", ImVec2(-1,ImGui::GetTextLineHeight()*10))) {
+        //         ImPlot::SetupAxes(nullptr, nullptr, flags, flags);
+        //         ImPlot::SetupAxisLimits(ImAxis_X1,t - history, t, ImGuiCond_Always);
+        //         ImPlot::SetupAxisLimits(ImAxis_Y1,0,1);
+        //         ImPlot::SetNextFillStyle(IMPLOT_AUTO_COL,0.5f);
+        //         ImPlot::PlotShaded("Mouse X", &sdata1.Data[0].x, &sdata1.Data[0].y, sdata1.Data.size(), -INFINITY, 0, sdata1.Offset, 2 * sizeof(float));
+        //         ImPlot::PlotLine("Mouse Y", &sdata2.Data[0].x, &sdata2.Data[0].y, sdata2.Data.size(), 0, sdata2.Offset, 2*sizeof(float));
+        //         ImPlot::EndPlot();
+        //     }
+        //     if (ImPlot::BeginPlot("##Rolling", ImVec2(-1,ImGui::GetTextLineHeight()*10))) {
+        //         ImPlot::SetupAxes(nullptr, nullptr, flags, flags);
+        //         ImPlot::SetupAxisLimits(ImAxis_X1,0,history, ImGuiCond_Always);
+        //         ImPlot::SetupAxisLimits(ImAxis_Y1,0,1);
+        //         ImPlot::PlotLine("Mouse X", &rdata1.Data[0].x, &rdata1.Data[0].y, rdata1.Data.size(), 0, 0, 2 * sizeof(float));
+        //         ImPlot::PlotLine("Mouse Y", &rdata2.Data[0].x, &rdata2.Data[0].y, rdata2.Data.size(), 0, 0, 2 * sizeof(float));
+        //         ImPlot::EndPlot();
+        //     }
+            
+        //     ImGui::End();
+        // }
